690a9bca8b15384a4d7fe9f64b54333451d64f3c
Abhiram Shibu <abhiramshibu1998@gmail.com>
2020-08-23 21:49:24 +0530

    Code optimized and Device ID added

--- Strap.ino
+++ Strap.ino
@@ -1,5 +1,12 @@
-#include<Arduino.h>
+#include <Arduino.h>
+#include <EEPROM.h> 
+#include <stdint.h>
+#include <avr/pgmspace.h>
+#include <avr/power.h>
+#include <LowPower.h>
+#define ID      "1be32f-e11"
 #define VERSION "1.0-alpha"
+#define RESETPIN 8
 #define commandDelay 9
 #define DEBUG true
 #define True true
@@ -7,6 +14,7 @@
 #define timeoutShort 10
 #define timeoutLong 200
 #define MESSAGELEN 100           // 100 Bytes message length
+#define data_byte_size 4         // EEPROM
 /* Global variables section */
 String command="";
 String responce="";
@@ -16,6 +24,8 @@ char buff[MESSAGELEN];
 char KEY[7]="123456";
 bool ENCEN=true;
 int keylen=6;
+int location=0;
+unsigned int counter=0;
 /* END Global variables */
 
 /* Global functions Section */
@@ -43,71 +53,69 @@ void enc(char * key, char * data, int datalen){
         poskey=(i+1)%keylen;
     }
 }
-/* END Global functions */
-
 void bluetoothInit(){
     //Set role as periferal
     int retry=5;
     while(True){
-        command="AT+ROLE0";
+        command=F("AT+ROLE0");
         sendCommAndGetResp(timeoutLong);
         if(DEBUG){
             Serial.println(responce);
         }
         retry--;
-        if(retry==0 || responce=="OK+Set:0"){
+        if(retry==0 || responce==F("OK+Set:0")){
             break;
         }
     }
     //Set autostart
     retry=5;
     while(True){
-        command="AT+IMME0";
+        command=F("AT+IMME0");
         sendCommAndGetResp(timeoutLong);
         if(DEBUG){
             Serial.println(responce);
         }
         retry--;
-        if(retry==0 || responce=="OK+Set:0"){
+        if(retry==0 || responce==F("OK+Set:0")){
             break;
         }
     }
     //Set Bluetooth Name
     retry=5;
     while(True){
-        command="AT+NAMEStrap";
+        command=F("AT+NAMEStrap");
         sendCommAndGetResp(timeoutLong);
         if(DEBUG){
             Serial.println(responce);
         }
         retry--;
-        if(retry==0 || responce=="OK+Set:Strap"){
+        if(retry==0 || responce==F("OK+Set:Strap")){
             break;
         }
     }
     //Set Bluetooth paring code
     retry=5;
     while(True){
-        command="AT+PASS123456";
+        command=F("AT+PASS123456");
         sendCommAndGetResp(timeoutLong);
         if(DEBUG){
             Serial.println(responce);
         }
         retry--;
-        if(retry==0 || responce=="OK+Set:123456"){
+        if(retry==0 || responce==F("OK+Set:123456")){
             break;
         }
     }
     retry=5;
     //Set Bluetooth paring not needed
     while(True){
-        command="AT+TYPE0";
+        command=F("AT+TYPE0");
         sendCommAndGetResp(timeoutLong);
         if(DEBUG){
             Serial.println(responce);
         }
         retry--;
-        if(retry==0 || responce=="OK+Set:0"){
+        if(retry==0 || responce==F("OK+Set:0")){
             break;
         }
     }
@@ -115,13 +123,13 @@ void bluetoothInit(){
     retry=5;
     //Set Bluetooth Service UUID
     while(True){
-        command="AT+UUID0x0003";
+        command=F("AT+UUID0x0003");
         sendCommAndGetResp(timeoutLong);
         if(DEBUG){
             Serial.println(responce);
         }
         retry--;
-        if(retry==0 || responce=="OK+Set:0x0003"){
+        if(retry==0 || responce==F("OK+Set:0x0003")){
             break;
         }
     }
@@ -129,17 +137,70 @@ void bluetoothInit(){
     // retry=5;
     // //Set Bluetooth power
     // while(True){
-    //     command="AT+POWE7";
+    //     command=F("AT+POWE7");
     //     sendCommAndGetResp(timeoutLong);
     //     if(DEBUG){
     //         Serial.println(responce);
     //     }
     //     retry--;
-    //     if(retry==0 || responce=="OK+Set:7"){
+    //     if(retry==0 || responce==F("OK+Set:7")){
     //         break;
     //     }
     // }
 }
+
+// EEPROM FUNCTIONS 
+int writeEEPROMCounter(unsigned int counter, int prev_location){
+    int new_location = (prev_location+data_byte_size)%1024;
+    int temp = 0xFF;
+    for(int i=new_location-1;i>=prev_location;i--){
+        uint8_t lsbyte = (counter & temp);
+        EEPROM.write(i,lsbyte);
+        counter = counter >> 8;
+    }
+    return new_location;
+}
+int readEEPROMCounter(int location){
+    unsigned int counter=0;
+    for(int i=location;i<location+data_byte_size;i++){
+        uint8_t lsbyte = EEPROM.read(i);
+        counter = counter | lsbyte;
+        if(i!=location+data_byte_size-1)
+            counter = counter<<8;
+    }
+    return counter;
+}
+void recoverCounter(unsigned int *counter, int *location){
+    *counter=0;
+    *location=0;
+    for(int i=0;i<256;i++){
+        unsigned int temp = readEEPROMCounter(i*data_byte_size);
+        if(temp>*counter){
+            *counter=temp;
+            *location=i;
+        }
+    }
+    *location*=data_byte_size;
+}
+void EEPROMInit(){
+    pinMode(RESETPIN,0);
+    digitalWrite(RESETPIN,1);
+    if(digitalRead(RESETPIN)==1){
+        for(int i=0;i<1024;i++){
+            EEPROM.write(i,0);
+        }
+        Serial.println(F("RST_SUC"));
+        delay(1);
+        LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+    }  
+    else{
+        recoverCounter(&counter,&location);
+        if(DEBUG==true){
+            Serial.println("CTR "+String(counter)+" POS "+String(location));
+        }
+    }
+}
+/* END Global functions */
 
 void setup() {
     if(DEBUG){
@@ -156,6 +217,8 @@ void setup() {
     Serial.begin(115200);
     command.reserve(20);
     responce.reserve(20);
+    //Initialize EEPROM if RESETPIN disconnected
+    EEPROMInit();
     //Initialize BT
     bluetoothInit();
     //Done Initialization
@@ -169,17 +232,21 @@ void loop(){
     
     Serial.setTimeout(timeoutShort);
     String temp=Serial.readString();
-    if(temp!=""){
-        if(temp=="VERSION"){
-            Serial.println(VERSION);
+    if(temp!=F("")){
+        if(temp==F("VERSION")){
+            Serial.println(F(VERSION));
         }
-        else if(temp=="ENCDIS"){
+        else if(temp==F("ENCDIS")){
             ENCEN=false;
         }
-        else if(temp=="ENCEN"){
+        else if(temp==F("ENCEN")){
             ENCEN=true;
         }
-        else if(temp.substring(0,3)=="ENC"){
+        else if(temp.substring(0,3)==F("ENC")){
+            temp+=String(F(":"));
+            temp+=F(ID);
+            temp+=String(F(":"));
+            temp+=String(counter);
             temp.substring(3).toCharArray(buff,MESSAGELEN);
             int len=strlen(buff);
             //Serial.write(buff);
@@ -193,6 +260,8 @@ void loop(){
                     command+="-";
             }
             sendCommAndGetResp(timeoutShort);
+            counter++;
+            location=writeEEPROMCounter(counter,location);
         }
     }
     //command="Hello World!";
