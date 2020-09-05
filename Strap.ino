5b5bc86b93b03f3f69ebab8ab21d01fe547385ac
Abhiram Shibu <abhiramshibu1998@gmail.com>
2020-09-05 18:13:51 +0530

    Strap Encryption AbhiCrypt but failure

--- Strap.ino
+++ Strap.ino
@@ -4,6 +4,8 @@
 #include <avr/pgmspace.h>
 #include <avr/power.h>
 #include <LowPower.h>
+#include <AESLib.h>
+#include <stdlib.h>
 #define ID      "1be32f-e11"
 #define VERSION "1.0-alpha"
 #define RESETPIN 8
@@ -21,7 +23,7 @@ String responce="";
 long heartBeatTimer;
 bool heartBeatVar=false;
 char buff[MESSAGELEN];
-char KEY[7]="123456";
+char KEY[]="1234567891234567891234567890123";
 bool ENCEN=true;
 int keylen=6;
 int location=0;
@@ -30,20 +32,111 @@ unsigned int counter=0;
 
 /* Global functions Section */
 /* Heart beat will help to see if the code is stuck or running */
-void heartBeat(){
-    if(DEBUG && (millis()-heartBeatTimer)>500){
-        digitalWrite(13,heartBeatVar);
-        heartBeatTimer=millis();
-        heartBeatVar=!heartBeatVar;
-    }
+//Encryption
+uint8_t sbox[32][2][2]={{{11,0},{31,1}}, // 1
+                        {{9,1},{26,0}},  // 2
+                        {{10,1},{2,1}},  // 3
+                        {{4,1},{7,0}},   // 4
+                        {{14,0},{29,1}}, // 5
+                        {{27,1},{11,0}}, // 6
+                        {{8,0},{21,1}},  // 7
+                        {{12,1},{8,0}},  // 8
+                        {{6,1},{22,0}},  // 9
+                        {{32,1},{6,1}},  // 10
+                        {{7,1},{30,0}},  // 11
+                        {{25,0},{18,0}}, // 12
+                        {{15,1},{28,0}}, // 13
+                        {{3,0},{16,1}},  // 14
+                        {{22,0},{19,0}}, // 15
+                        {{18,1},{17,0}}, // 16
+                        {{2,1},{32,0}},  // 17
+                        {{1,0},{24,1}},  // 18 
+                        {{24,0},{14,1}}, // 19
+                        {{28,0},{10,0}}, // 20
+                        {{26,0},{9,1}},  // 21
+                        {{13,1},{13,0}}, // 22
+                        {{30,1},{1,1}},  // 23
+                        {{17,1},{20,1}}, // 24
+                        {{19,1},{12,0}}, // 25
+                        {{5,1},{5,0}},   // 26
+                        {{21,0},{23,1}}, // 27
+                        {{23,1},{25,0}}, // 28
+                        {{31,0},{15,1}}, // 29
+                        {{29,0},{3,0}},  // 30
+                        {{20,0},{27,1}}, // 31
+                        {{16,1},{4,0}}};  // 32
+uint8_t swapbox[32][2][2]={{{23,1},{20,32}},  //1
+                           {{7,8},{6,5}},     //2
+                           {{8,29},{19,22}},  //3
+                           {{29,11},{24,27}}, //4
+                           {{9,10},{18,2}},   //5
+                           {{25,26},{17,26}}, //6
+                           {{4,17},{9,23}},   //7
+                           {{6,19},{3,29}},   //8
+                           {{2,24},{2,18}},   //9
+                           {{16,30},{10,11}}, //10
+                           {{27,28},{13,20}}, //11
+                           {{18,32},{4,12}},  //12
+                           {{15,20},{15,16}}, //13
+                           {{3,22},{14,21}},  //14
+                           {{5,6},{1,30}},    //15
+                           {{13,25},{7,31}},  //16
+                           {{14,31},{26,3}},  //17
+                           {{21,12},{11,17}}, //18
+                           {{31,2},{12,24}},  //19
+                           {{11,23},{5,28}},  //20
+                           {{32,3},{8,14}},   //21
+                           {{10,13},{25,4}},  //22
+                           {{12,14},{22,6}},  //23
+                           {{19,21},{27,19}}, //24
+                           {{20,4},{23,25}},  //25
+                           {{17,15},{16,13}}, //26
+                           {{22,5},{21,7}},   //27
+                           {{24,7},{28,8}},   //28
+                           {{26,27},{32,1}},  //29
+                           {{28,9},{31,10}},  //30
+                           {{1,18},{30,15}},  //31
+                           {{30,16},{29,9}}}; //32
+// above is a list { { {falseSwap1,falseSwap2}, {trueSwap1,trueSwap2} } }
+uint32_t setbit(uint32_t val,uint8_t pos,uint8_t setval){
+    if(setval==0)
+        val = val & (~(1<<pos));
+    else
+        val = val | (1<<pos);
+    return val;
 }
-void sendCommAndGetResp(unsigned int timeout){
-    delay(commandDelay);
-    command.toCharArray(buff,MESSAGELEN);
-    Serial.write(buff);
-    delay(commandDelay);
-    Serial.setTimeout(timeout);
-    responce=Serial.readString();
+uint8_t getbit(uint32_t val,uint8_t pos){
+    if((val&(1<<pos))==(1<<pos))
+        return 1;
+    else
+        return 0;
+}
+uint32_t swapbits(uint32_t val,uint8_t index1,uint8_t index2){
+    uint8_t bit1=0;
+    uint8_t bit2=0;
+    uint32_t temp=1<<index1;
+    if(temp == (val & temp))
+        bit1=1;
+    temp=1<<index2;
+    if(temp == (val & temp))
+        bit2=1;
+    val=setbit(val,index1,bit2);
+    val=setbit(val,index2,bit1);
+    return val;
+}
+uint32_t seedKey(uint32_t seed,uint8_t Key){
+    uint32_t key = Key * 4;
+    for(int i=0;i<32;i++){
+        if(getbit(seed,i)==1){
+            key=setbit(key,sbox[i][1][0]-1,sbox[i][1][1]);
+            key=swapbits(key,swapbox[i][1][0]-1,swapbox[i][1][1]-1);
+        }
+        else{
+            //key=setbit(key,sbox[i][0][0]-1,sbox[i][0][1]);
+            key=swapbits(key,swapbox[i][0][0]-1,swapbox[i][0][1]-1);
+        }
+    }
+    return key/4;
 }
 void enc(char * key, char * data, int datalen){
     int poskey=0;
@@ -52,6 +145,23 @@ void enc(char * key, char * data, int datalen){
         data[i]=data[i]^key[poskey];
         poskey=(i+1)%keylen;
     }
+}
+//Generic
+void heartBeat(){
+    if(DEBUG && (millis()-heartBeatTimer)>500){
+        digitalWrite(13,heartBeatVar);
+        heartBeatTimer=millis();
+        heartBeatVar=!heartBeatVar;
+    }
+}
+//Bluetooth
+void sendCommAndGetResp(unsigned int timeout){
+    delay(commandDelay);
+    command.toCharArray(buff,MESSAGELEN);
+    Serial.write(buff);
+    delay(commandDelay);
+    Serial.setTimeout(timeout);
+    responce=Serial.readString();
 }
 void bluetoothInit(){
     //Set role as periferal
@@ -248,11 +358,14 @@ void loop(){
             temp+=String(F(":"));
             temp+=String(counter);
             temp.substring(3).toCharArray(buff,MESSAGELEN);
-            int len=strlen(buff);
+            int len=MESSAGELEN;
             //Serial.write(buff);
             if(ENCEN)
-                enc(KEY,buff,len);
+                //enc(KEY,buff,len);
+                //Bringup AES
+                aes256_enc_single((uint8_t *)KEY,buff);
             command="";
+            srand(100);
             //Serial.print(strlen(buff));
             for(int i=0;i<len;i++){
                 command+=String(buff[i],HEX);
